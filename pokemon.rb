5c51c26fa93b748d5b1272bb21a3717df778aa57
jordanoboyle <jmcmcode@gmail.com>
2024-05-12 23:24:21 -0600

    created list of all pokemon, includes special

--- pokemon.rb
+++ pokemon.rb
@@ -27,3 +27,14 @@ end
 
 # p which_version
 
+poke_list = (HTTP.get("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0").parse)["results"].map {|species| species["name"]}
+p poke_list
+
+
+# poke_list["results"]
+# i = 0
+# while i < poke_list["results"].length
+#   species = poke_list["results"]
+#   p species[i]["name"]
+#   i += 1
+# end
