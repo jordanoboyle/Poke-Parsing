1ee1f441ea780e1a07b46404894bb487fa811786
jordanoboyle <jmcmcode@gmail.com>
2024-05-12 23:38:54 -0600

    Dried up versions appeard with map loop.

--- pokemon.rb
+++ pokemon.rb
@@ -10,25 +10,25 @@ require "http"
 # p pokemon_info["game_indices"][0]["version"]["name"]
 
 # p '------'
+poke_list = (HTTP.get("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0").parse)["results"].map {|species| species["name"]}
+# p poke_list
 
 def which_version()
   p "Choose a pokemon: "
   pokemon = gets.chomp.downcase
+  
   pokemon_info = HTTP.get("https://pokeapi.co/api/v2/pokemon/#{pokemon}/").parse
   
-  
-  versions_appeared = []
-  pokemon_info["game_indices"].each do |element|
-    version = element["version"]["name"]
-    versions_appeared << version
-  end
+  versions_appeared = (HTTP.get("https://pokeapi.co/api/v2/pokemon/totodile/").parse)["game_indices"].map {|element| element["version"]["name"]}
+ 
   return "#{pokemon.capitalize} appeared in these in: #{versions_appeared.join(", ")}"
 end
 
-# p which_version
+# dry up versions appeared...
+# map and print version
 
-poke_list = (HTTP.get("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0").parse)["results"].map {|species| species["name"]}
-p poke_list
+
+
 
 
 # poke_list["results"]
